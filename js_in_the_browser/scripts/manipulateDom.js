6abdbffd85f49c05f005da7e2b0fe57cc2d26a46
rgirodon <remy.girodon@gmail.com>
2017-11-14 11:02:22 -0500

    add manipulateDom tp

---
+++ js_in_the_browser/scripts/manipulateDom.js
@@ -0,0 +1,26 @@
+var link = document.querySelector('a');
+
+link.textContent = 'Mozilla Developer Network';
+
+link.href = 'https://developer.mozilla.org';
+
+var sect = document.querySelector('section');
+
+var para = document.createElement('p');
+
+para.textContent = 'We hope you enjoyed the ride.';
+
+para.setAttribute('class', 'highlight');
+
+sect.appendChild(para);
+
+var text = document.createTextNode(' — the premier source for web development knowledge.');
+
+var linkPara = document.querySelector('p');
+
+linkPara.appendChild(text);
+
+sect.appendChild(linkPara);
+
+// linkPara.parentNode.removeChild(linkPara);
+
