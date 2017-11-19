33e2b0853bd56e2f3a260d6990c98a6f4b4dd76f
rgirodon <remy.girodon@gmail.com>
2017-11-19 13:11:11 -0500

    Debut ajax

--- intro_nodejs/helloWorld.js
+++ intro_nodejs/helloWorld.js
@@ -1,7 +1,31 @@
-let hello = "Hello";
+let user = {
+    name: "John",
+    age: 25
+};
 
-let prenom = 'Remy';
+console.log(user.name);
 
-let message = `${hello} ${prenom} :)`;
+console.log(user.age);
 
-console.log(message);
+user.name = "Peter";
+
+user.age = 17;
+
+console.log(user.name + " a " + user.age);
+
+user.address = "83 Rue de la République 42100 St-Etienne";
+
+console.log(user.address);
+
+let user2 = {
+    name: "John",
+    age: 25,
+    address: {
+        numero: 83,
+        rue: "Rue de la République",
+        codePostal : 42100,
+        ville : "St-Etienne"
+    }
+};
+
+console.log(user2.address.codePostal);
